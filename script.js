e98d09b47289afc3ee7df5b7e8b2a9bfe86fb4fd
xent <75753443+unknownxent@users.noreply.github.com>
2024-08-18 21:07:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
